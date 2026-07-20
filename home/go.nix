acd2db112abe6e9f837cb91b205af598b7cf6d53
Nikita Galaiko <nikita@galaiko.rocks>
2026-07-20 10:07:56 +0200

    drop old go

--- home/go.nix
+++ home/go.nix
@@ -2,13 +2,8 @@
 {
   home.packages = [ pkgs.go ];
 
-  # /opt/go is the pre-existing mac GOPATH; go's default elsewhere.
   programs.fish.shellInit = ''
-    if test -d /opt/go
-      set --global --export GOPATH "/opt/go"
-    else
-      set --global --export GOPATH "$HOME/go"
-    end
+    set --global --export GOPATH "$HOME/go"
     fish_add_path --global --move --path "$GOPATH/bin"
   '';
 }
