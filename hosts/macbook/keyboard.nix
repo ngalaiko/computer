f8283174cae5fa1ef83029f21d3f2de82055c5db
Nikita Galaiko <nikita@galaiko.rocks>
2026-08-26 10:30:37 +0200

    configure input sources

--- hosts/macbook/keyboard.nix
+++ hosts/macbook/keyboard.nix
@@ -22,6 +22,28 @@
     NSUserDictionaryReplacementItems = [ ];
   };
 
+  # enabled input sources: U.S. + Russian keyboard layouts
+  system.defaults.CustomUserPreferences."com.apple.HIToolbox".AppleEnabledInputSources = [
+    {
+      InputSourceKind = "Keyboard Layout";
+      "KeyboardLayout ID" = 0;
+      "KeyboardLayout Name" = "U.S.";
+    }
+    {
+      "Bundle ID" = "com.apple.CharacterPaletteIM";
+      InputSourceKind = "Non Keyboard Input Method";
+    }
+    {
+      InputSourceKind = "Keyboard Layout";
+      "KeyboardLayout ID" = 19456;
+      "KeyboardLayout Name" = "Russian";
+    }
+    {
+      "Bundle ID" = "com.apple.PressAndHold";
+      InputSourceKind = "Non Keyboard Input Method";
+    }
+  ];
+
   system.defaults.CustomUserPreferences."com.apple.symbolichotkeys".AppleSymbolicHotKeys = {
     # Select the previous input source — remapped to Cmd+Space.
     "60" = {
